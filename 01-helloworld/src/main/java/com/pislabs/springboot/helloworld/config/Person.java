df6badd98012d9e3927d293fca5beef59aac38cb
rayl <liuyi86@zto.com>
2025-06-09 06:36:43 +0800

    feat: configuration

---
+++ 01-helloworld/src/main/java/com/pislabs/springboot/helloworld/config/Person.java
@@ -0,0 +1,6 @@
+package com.pislabs.springboot.helloworld.config;/**
+ * @author rayl
+ * @description: 
+ * @date 2025/6/7 03:13
+ */public class Person {
+}
---
+++ 01-helloworld/src/test/java/com/pislabs/springboot/helloworld/SprintBootConfigTests.java
@@ -0,0 +1,6 @@
+package com.pislabs.springboot.helloworld;/**
+ * @author rayl
+ * @description: 
+ * @date 2025/6/7 03:24
+ */public class SprintBootConfigTests {
+}
