7ab9fe00edf973dfa1380303d454ce465dd7fc66
rayl <liuyi86@zto.com>
2025-06-12 06:36:30 +0800

    feat: logging

---
+++ 02-logging/src/main/java/com/pislabs/springboot/logging/LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.pislabs.springboot.logging;
+
+/**
+ * @author rayl
+ * @description:
+ * @date $DATE $HOUR:$MINUTE
+ */
+@SpringBootApplication
+public class LoggingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigQApplication.class, args);
+    }
+}
